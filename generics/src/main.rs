23f5af5312dc9472b9f11912deed0afdfa53f6bb
Gabe <gabe@gabesaba.com>
2019-03-07 23:44:23 -0500

    Generic Data Types (10.1)

---
+++ generics/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    println!("Hello, world!");
+    let a = vec![1, 2, 3, 4, 5];
+    println!("{}", largest(&a));
+
+    // This wont work because Point expects same type
+    // let point = Point{x: 5, y: -5.0};
+
+    let point = Point{x: 0, y: 5};
+    let h_point = HeterogeneousPoint{ x: 5, y: -5.0};
+}
+
+fn largest<T>(iter: &[T]) -> T {
+
+    let mut largest = iter[0];
+    for i in iter.iter() {
+        if i > largest {
+            largest = *i
+        }
+    }
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+struct HeterogeneousPoint<T, V> {
+    x: T,
+    y: V,
+}
+
+// From Book
+impl<T, V> HeterogeneousPoint<T, V> {
+    fn mixup<U, W>(self, other: HeterogeneousPoint<U, W>) -> HeterogeneousPoint<T, W> {
+        HeterogeneousPoint {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+enum Option<T> {
+    Some(T),
+    None,
+}
