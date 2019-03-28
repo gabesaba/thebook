72f7b8702e6259523ec4214cff4dede7d135817f
Gabe <dev@gabesaba.com>
2019-03-27 23:44:24 -0400

    Release Profiles

---
+++ release_profiles/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("We can define release profiles in our Cargo.toml to control how code is compiled. Opt-level can be set between 0-3, where 3 is the most optimized. I selected 2 for dev and 3 for release.");
+}
