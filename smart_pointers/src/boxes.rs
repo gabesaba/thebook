c8db7b6fed2ca76329c8bc535a1c5b7949379ffc
Gabe <dev@gabesaba.com>
2019-03-31 17:00:08 -0400

    Deref trait

---
+++ smart_pointers/src/boxes.rs
@@ -0,0 +1,51 @@
+
+struct NaturalNumber {
+    natural_number: Option<Box<NaturalNumber>>,
+}
+
+impl NaturalNumber {
+    fn new(num: u32) -> NaturalNumber {
+        if num == 0 {
+            NaturalNumber {
+                natural_number: None,
+            }
+        } else {
+            NaturalNumber {
+                natural_number: Some(Box::new(NaturalNumber::new(num - 1))),
+            }
+        }
+    }
+
+    fn val(&self) -> u32 {
+        let mut as_u32 = 0;
+        let mut natural_number = &self.natural_number;
+        loop {
+            match natural_number {
+                Some(n) => {
+                    as_u32 += 1;
+                    natural_number = &n.natural_number;
+                }
+                None => return as_u32,
+            }
+        }
+    }
+}
+
+pub fn boxes() {
+    let b = Box::new(42);
+    let a = NaturalNumber::new(*b);
+    println!("Created new natural number of size {}", *b);
+    println!("Val of a: {}", a.val());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::NaturalNumber;
+    #[test]
+    fn test_natural_nums() {
+        for i in 0..1000 {
+            let a = NaturalNumber::new(i);
+            assert_eq![i, a.val()];
+        }
+    }
+}
---
+++ smart_pointers/src/deref.rs
@@ -0,0 +1,39 @@
+use std::ops::Deref;
+
+pub fn deref() {
+    let x = 42;
+    let y = MyBox::new(x);
+
+    assert_eq!(42, *y);
+
+    // *y is equivalent to *(y.deref())
+    assert_eq!(*y, *(y.deref()));
+
+    let name = MyBox::new(String::from("Rafi"));
+    hello(&name);
+}
+
+// Deref coercion
+fn hello(name: &str) {
+    println!("Hello {}", name);
+}
+
+struct MyBox<T>(T);
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+
+
+
